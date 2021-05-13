74c2934f577ab9c7207bb400a6b2e503e2ad7b85
NurseyitTursunkulov <nurseyit.tursunkulov@gmail.com>
2021-05-13 16:43:54 +0600

    added splash screen

---
+++ app/src/main/java/com/example/hellocompose/ui/SplashScreen.kt
@@ -0,0 +1,23 @@
+package com.example.hellocompose.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.example.hellocompose.R
+
+@Composable
+fun splashScreen(){
+    Column(
+        modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_logo),
+            contentDescription = "null" // decorative element
+        )
+    }
+}
