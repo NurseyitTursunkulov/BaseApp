03d18d21b663242cc7c49a3764c98eb5b6dfa327
NurseyitTursunkulov <nurseyit.tursunkulov@gmail.com>
2021-05-13 16:57:55 +0600

    added util classes

--- app/src/main/java/com/example/hellocompose/ui/util/util.kt
+++ app/src/main/java/com/example/hellocompose/ui/util/util.kt
@@ -8,6 +8,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.OffsetMapping
+import androidx.compose.ui.text.input.TransformedText
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
@@ -75,4 +79,44 @@ fun createBottomNavBar(
             )
         }
     }
+}
+
+class NumberTransformation() : VisualTransformation {
+    override fun filter(text: AnnotatedString): TransformedText {
+        return numberFilter(text)
+    }
+}
+
+fun numberFilter(text: AnnotatedString): TransformedText {
+
+    // +XXX XXX XXX XXX
+    val trimmed = if (text.text.length >= 12) text.text.substring(0..11) else text.text
+    var out = ""
+    for (i in trimmed.indices) {
+        if (i==0) out += "+"
+        out += trimmed[i]
+        if (i % 3 == 2 && i != 11) out += " "
+    }
+
+    val numberOffsetTranslator = object : OffsetMapping {
+        override fun originalToTransformed(offset: Int): Int {
+            if (offset <= 0) return offset
+            if (offset <= 2) return offset +1
+            if (offset <= 5) return offset +2
+            if (offset <= 8) return offset +3
+            if (offset <= 12) return offset +4
+            return 16
+        }
+
+        override fun transformedToOriginal(offset: Int): Int {
+            if (offset <=0) return offset
+            if (offset <=3) return offset -1
+            if (offset <=7) return offset -2
+            if (offset <=11) return offset -3
+            if (offset <=15) return offset -4
+            return 11
+        }
+    }
+
+    return TransformedText(AnnotatedString(out), numberOffsetTranslator)
 }
