c953c4f220bf60ee24e60d6c9df9c89741ee2e49
NurseyitTursunkulov <nurseyit.tursunkulov@gmail.com>
2021-05-13 17:14:40 +0600

    added modules to koin di

---
+++ app/src/main/java/com/example/hellocompose/di/ApiModules.kt
@@ -0,0 +1,60 @@
+package com.example.hellocompose.di
+
+import android.os.UserManager
+import com.example.hellocompose.data.login.ApiService
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.timelysoft.shelter.android.util.network.NetworkResponseAdapterFactory
+import okhttp3.*
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.dsl.module
+import org.koin.java.KoinJavaComponent.get
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+val apiModules = module {
+
+    factory<Gson> {
+        GsonBuilder().setLenient().create()
+    }
+    single<OkHttpClient> {
+        OkHttpClient().newBuilder()
+            .addInterceptor(Interceptor { chain ->
+                val newUrl = chain.request().url
+                    .newBuilder()
+                    .build()
+
+                val newRequest = chain.request()
+                    .newBuilder()
+//                    .addHeader("Authorization", "bearer " + AppPreferences.token)
+                    .url(newUrl)
+                    .build()
+
+                chain.proceed(newRequest)
+            })
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .writeTimeout(90, TimeUnit.SECONDS)
+            .build()
+    }
+    single<Retrofit> {
+        Retrofit.Builder()
+            .client(
+                get()
+            )
+            .addCallAdapterFactory(NetworkResponseAdapterFactory())
+            .addConverterFactory(GsonConverterFactory.create(get()))
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .baseUrl("http://54.36.192.142:8073/api/")
+            .build()
+    }
+
+    single<ApiService> {
+        get<Retrofit>().create(ApiService::class.java)
+    }
+
+
+}
+
